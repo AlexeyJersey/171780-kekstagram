08793ac71c6b2dad771a9168a8fa0c9fd74aa61e
https://github.com/AlexeyJersey <alexeimakarow@gmail.com>
2017-03-30 14:39:07 +0200

    picture array generator

--- js/pictures.js
+++ js/pictures.js
@@ -1,18 +1,19 @@
 // picture.js
 'use strict';
 
-var pictureAdresses = 'pictures/{{' + numberGenerator(1, 25) + '}}.jpg';
+var pictureAdresses = 'pictures/{{' + numGenerator(1, 25) + '}}.jpg';
 var pictureComments = ['Всё отлично!', 'В целом всё неплохо. Но не всё.', 'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце-концов это просто непрофессионально.', 'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.', 'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.', 'Лица у людей на фотке перекошены, как-будто их избивают. Как можно было поймать такой неудачный момент?!'];
-var pictureLikes = numberGenerator(15, 200);
+var pictureLikes = numGenerator(15, 200);
+var pictureArray = [];
 
-function numberGenerator(minNumber, maxNumber) {
+function numGenerator(minNumber, maxNumber) {
   var numberRandom = Math.random() * (maxNumber - minNumber);
   numberRandom = numberRandom.toFixed();
   return numberRandom;
 }
 
 function takeAnyValueFromArray(array) {
-  return array[numberGenerator(0, array.length - 1)];
+  return array[numGenerator(0, array.length - 1)];
 }
 
 function pictureGenerator() {
@@ -23,5 +24,8 @@ function pictureGenerator() {
   };
 }
 
+for (var i = 0; i < 25; i++) {
+  pictureArray[i] = pictureGenerator();
+}
 
-console.log(pictureGenerator());
+console.log(pictureArray);
