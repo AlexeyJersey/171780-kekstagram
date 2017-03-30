5c122c831ad696ca2491c22bdb982b3dfdff8b7c
https://github.com/AlexeyJersey <alexeimakarow@gmail.com>
2017-03-30 14:21:48 +0200

    picture array generator

--- js/pictures.js
+++ js/pictures.js
@@ -1,2 +1,27 @@
 // picture.js
 'use strict';
+
+var pictureAdresses = 'pictures/{{' + numberGenerator(1, 25) + '}}.jpg';
+var pictureComments = ['Всё отлично!', 'В целом всё неплохо. Но не всё.', 'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце-концов это просто непрофессионально.', 'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.', 'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.', 'Лица у людей на фотке перекошены, как-будто их избивают. Как можно было поймать такой неудачный момент?!'];
+var pictureLikes = numberGenerator(15, 200);
+
+function numberGenerator(minNumber, maxNumber) {
+  var numberRandom = Math.random() * (maxNumber - minNumber);
+  numberRandom = numberRandom.toFixed();
+  return numberRandom;
+}
+
+function takeAnyValueFromArray(array) {
+  return array[numberGenerator(0, array.length - 1)];
+}
+
+function pictureGenerator() {
+  return {
+    url: pictureAdresses,
+    likes: pictureLikes,
+    comments: takeAnyValueFromArray(pictureComments)
+  };
+}
+
+
+console.log(pictureGenerator());
