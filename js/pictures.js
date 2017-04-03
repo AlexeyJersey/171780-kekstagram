b39ee401e8d0eb2985bc1c50662941537471f786
https://github.com/AlexeyJersey <alexeimakarow@gmail.com>
2017-04-03 17:55:56 +0200

    picture.js refactoring

--- js/pictures.js
+++ js/pictures.js
@@ -1,90 +1,100 @@
-// picture.js
+// pictures-re.js
 'use strict';
 
-// picture data
-var pictureDataArr = [];
+var photoIds = fillPhotoIds();
+var commentsCollection = ['Всё отлично!', 'В целом всё неплохо. Но не всё.', 'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце-концов это просто непрофессионально.', 'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.', 'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.', 'Лица у людей на фотке перекошены, как-будто их избивают. Как можно было поймать такой неудачный момент?!'];
+var pictureObjectCollection = generatePictureCollection();
+
+function picturesShow() {
+  var pictureTemplate = document.querySelector('#picture-template').content;
+  var pictureList = document.querySelector('.pictures');
+  var pictureListFragment = document.createDocumentFragment();
+
+  for (var k = 0; k < generatePictureCollection().length; k++) {
+    var pictureElement = pictureTemplate.cloneNode(true);
+
+    pictureElement.querySelector('img').src = pictureObjectCollection[k].url;
+    pictureElement.querySelector('.picture-likes').textContent = pictureObjectCollection[k].likes;
+    pictureElement.querySelector('.picture-comments').textContent = pictureObjectCollection[k].comments.length;
+
+    pictureListFragment.appendChild(pictureElement);
+  }
+
+  pictureList.appendChild(pictureListFragment);
+}
+
+function galleryOverlayShow() {
+  var galleryOverlayElement = document.querySelector('.gallery-overlay');
+  galleryOverlayElement.classList.remove('invisible');
+  uploadOverlayHide();
+
+  galleryOverlayElement.querySelector('.gallery-overlay-image').src = pictureObjectCollection[0].url;
+  galleryOverlayElement.querySelector('.likes-count').textContent = pictureObjectCollection[0].likes;
+  galleryOverlayElement.querySelector('.comments-count').textContent = pictureObjectCollection[0].comments.length;
+}
+
+function uploadOverlayHide() {
+  document.querySelector('.upload-overlay').classList.add('invisible');
+}
+
+function galleryOverlayHide() {
+  document.querySelector('.gallery-overlay').classList.add('invisible');
+}
+
+picturesShow();
+galleryOverlayShow();
+
+function generatePictureCollection() {
+  var pictureCollection = [];
+  for (var j = 0; j < 25; j++) {;
+    pictureCollection.push(pictureGenerator());
+  }
+  return pictureCollection;
+}
 
 function pictureGenerator() {
   return {
-    url: pictureNorepeatUrl(),
-    likes: likesNumGen,
-    comments: commentsQty()
+    url: generatePhotoUrl(),
+    likes: numGen(15, 200),
+    comments: generateComment()
   };
 }
 
-// url
-var urlArr = []; // 'photos/' + i + '.jpg';
-
-function pictureNorepeatUrl() {
-  urlArr = shuffle(urlArr);
-  if (urlArr.length) {
-    return urlArr.pop();
+function generateComment() {
+  var singleComment;
+  var doubleComment;
+  var singleSentence = commentsCollection[numGen(0, commentsCollection.length - 1)];
+  var doubleSentence = commentsCollection[numGen(0, commentsCollection.length - 1)] + ' ' + commentsCollection[numGen(0, commentsCollection.length - 1)];
+  if (numGen(0, 1) === 0) {
+    return singleComment = {
+      text: singleSentence,
+      length: 1
+    };
   } else {
-    return null;
+    return doubleComment = {
+      text: doubleSentence,
+      length: 2
+    };
   }
 }
 
-// comments
-var commentsTextArray = ['Всё отлично!', 'В целом всё неплохо. Но не всё.', 'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце-концов это просто непрофессионально.', 'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.', 'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.', 'Лица у людей на фотке перекошены, как-будто их избивают. Как можно было поймать такой неудачный момент?!'];
-var randomComment = commentsTextArray[numGenerator(0, commentsTextArray.length - 1)];
-var commentSingle = randomComment;
-var commentDouble = randomComment + ' ' + randomComment;
+function generatePhotoUrl() {
+  return 'photos/' + photoIds.pop() + '.jpg';
+}
 
-function commentsQty() {
-  if (numGenerator(0, 1) === 0) {
-    commentSingle = 1;
-    return commentSingle;
-  } else {
-    commentDouble = 2;
-    return commentDouble;
+function fillPhotoIds() {
+  var idCollection = [];
+  for (var i = 1; i < 26; i++) {;
+    idCollection.push(i);
   }
+  return idCollection.sort(function (a, b) {
+    return Math.random() > 0.5 ? 1 : -1;
+  });
 }
 
-//likes
-var likesNumGen = numGenerator(15, 200);
-
-//arr fill
-for (var k = 1; k < 26; k++) {
-  urlArr.push('photos/' + k + '.jpg');
-  pictureDataArr.push(pictureGenerator());
-}
-
-// DOM bg pictures
-var pictureTemplate = document.querySelector('#picture-template').content;
-var pictureList = document.querySelector('.pictures');
-var pictureListFragment = document.createDocumentFragment();
-
-for (var l = 0; l < pictureDataArr.length; l++) {
-  var pictureElement = pictureTemplate.cloneNode(true);
-
-  pictureElement.querySelector('img').src = pictureDataArr[l].url;
-  pictureElement.querySelector('.picture-likes').textContent = pictureDataArr[l].likes;
-  pictureElement.querySelector('.picture-comments').textContent = pictureDataArr[l].comments;
-
-  pictureListFragment.appendChild(pictureElement);
-}
-
-pictureList.appendChild(pictureListFragment);
-
-// DOM remove upload element
-document.querySelector('.upload-overlay').classList.add('invisible');
-
-// DOM gallery img
-var galleryOverlayElement = document.querySelector('.gallery-overlay');
-galleryOverlayElement.classList.remove('invisible');
-
-galleryOverlayElement.querySelector('.gallery-overlay-image').src = pictureDataArr[numGenerator(0, pictureDataArr.length)].url;
-galleryOverlayElement.querySelector('.likes-count').textContent = pictureDataArr[numGenerator(0, pictureDataArr.length)].likes;
-galleryOverlayElement.querySelector('.comments-count').textContent = pictureDataArr[numGenerator(0, pictureDataArr.length)].comments;
-
-function numGenerator(minNumber, maxNumber) {
-  var numberRandom = Math.random() * (maxNumber - minNumber);
+function numGen(min, max) {
+  var numberRandom = Math.random() * (max - min);
   numberRandom = numberRandom.toFixed();
-  return numberRandom;
+  return +numberRandom;
 }
 
-function shuffle(array) {
-  for (var j, x, i = array.length; i; j = parseInt(Math.random() * i), x = array[--i], array[i] = array[j], array[j] = x); {
-    return array;
-  }
-}
