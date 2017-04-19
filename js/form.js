fcae4cfa90cad6f6afa5e710e97ea45d02d9abf1
https://github.com/AlexeyJersey <alexeimakarow@gmail.com>
2017-04-19 20:27:52 +0200

    initialize-scale.js module touched

--- js/form.js
+++ js/form.js
@@ -91,8 +91,8 @@
   uploadFormCommentsProperties();
   onSendButtonClick();
   resetPinPosition();
+  onControlButtonClick(scaleElement, scaleOptions);
 
-  window.initializeScale.resizeElement(scaleElement, scaleOptions);
 
   uploadFilterLevel.classList.add('invisible');
 
@@ -187,6 +187,34 @@
       uploadOverlayHide();
     }
   });
+
+  function onControlButtonClick(element, options) {
+
+    element.scaleValueField.value = options.maxValue + '%';
+    var scaleValue = Number(element.scaleValueField.value.slice(0, -1));
+    element.targetElement.style.transform = 'scale(' + scaleValue / options.maxValue + ')';
+
+    element.minusButton.addEventListener('click', function (evt) {
+      if (evt.target === element.minusButton && scaleValue >= (options.step * 2)) {
+        scaleValue = scaleValue - options.step;
+      } else {
+        scaleValue = options.step;
+      }
+      element.scaleValueField.value = scaleValue + '%';
+      element.targetElement.style.transform = 'scale(' + scaleValue / options.maxValue + ')';
+    });
+
+    element.plusButton.addEventListener('click', function (evt) {
+      if (evt.target === element.plusButton && scaleValue <= (options.maxValue - options.step)) {
+        scaleValue = scaleValue + options.step;
+      } else {
+        scaleValue = options.maxValue;
+      }
+      element.scaleValueField.value = scaleValue + '%';
+      element.targetElement.style.transform = 'scale(' + scaleValue / options.maxValue + ')';
+    });
+
+};
 
   function resetPinPosition() {
     uploadFilterLevelPin.style.left = 100 + '%';
