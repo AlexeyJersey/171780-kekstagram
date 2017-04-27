e1b256ac5dc1859a02904ba7fe21d8c4ea4c7a2b
https://github.com/AlexeyJersey <alexeimakarow@gmail.com>
2017-04-27 15:55:07 +0200

    B10 npm test

--- js/initialize-filters.js
+++ js/initialize-filters.js
@@ -1,26 +1,26 @@
 // initialize-filters.js
 'use strict';
 
-(function(){
+(function () {
 
-window.onFilterControlClick = function (targetNode, cb) {
-  var imgPreview = document.querySelector('.filter-image-preview');
-  var filterControls = document.querySelector('.upload-filter-controls');
+  window.onFilterControlClick = function (targetNode, cb) {
+    var imgPreview = document.querySelector('.filter-image-preview');
+    var filterControls = document.querySelector('.upload-filter-controls');
 
-  filterControls.addEventListener('click', function (evt) {
-    var target = evt.target;
+    filterControls.addEventListener('click', function (evt) {
+      var target = evt.target;
 
-    if (target.tagName === 'INPUT') {
-      var newFilter = 'filter-' + target.value;
-    }
+      if (target.tagName === 'INPUT') {
+        var newFilter = 'filter-' + target.value;
+      }
 
-    if (imgPreview.classList[1]) {
-      var oldFilter = imgPreview.classList[1];
-    }
+      if (imgPreview.classList[1]) {
+        var oldFilter = imgPreview.classList[1];
+      }
 
-    cb(newFilter, oldFilter);
-  });
+      cb(newFilter, oldFilter);
+    });
 
-};
+  };
 
 })();
--- js/initialize-scale.js
+++ js/initialize-scale.js
@@ -1,38 +1,38 @@
 // initialize-scale.js
 'use strict';
 
-(function(){
+(function () {
 
-window.onScaleControlsClick = function (targetNode, cb) {
-  var buttonMinus = document.querySelector('.upload-resize-controls-button-dec');
-  var buttonPlus = document.querySelector('.upload-resize-controls-button-inc');
-  var elementValue = document.querySelector('.upload-resize-controls-value');
+  window.onScaleControlsClick = function (targetNode, cb) {
+    var buttonMinus = document.querySelector('.upload-resize-controls-button-dec');
+    var buttonPlus = document.querySelector('.upload-resize-controls-button-inc');
+    var elementValue = document.querySelector('.upload-resize-controls-value');
 
-  var basicValue = 100 + '%';
-  elementValue.value = basicValue;
-  var scaleValue = Number(basicValue.slice(0, -1));
+    var basicValue = 100 + '%';
+    elementValue.value = basicValue;
+    var scaleValue = Number(basicValue.slice(0, -1));
 
-  buttonMinus.addEventListener('click', function (evt) {
-    if (evt.target === buttonMinus && scaleValue >= 50) {
-      scaleValue = scaleValue - 25;
-    } else {
-      scaleValue = 25;
-    }
+    buttonMinus.addEventListener('click', function (evt) {
+      if (evt.target === buttonMinus && scaleValue >= 50) {
+        scaleValue = scaleValue - 25;
+      } else {
+        scaleValue = 25;
+      }
+
+      cb(targetNode, scaleValue);
+    });
+
+    buttonPlus.addEventListener('click', function (evt) {
+      if (evt.target === buttonPlus && scaleValue <= 75) {
+        scaleValue = scaleValue + 25;
+      } else {
+        scaleValue = 100;
+      }
+
+      cb(targetNode, scaleValue);
+    });
 
     cb(targetNode, scaleValue);
-  });
-
-  buttonPlus.addEventListener('click', function (evt) {
-    if (evt.target === buttonPlus && scaleValue <= 75) {
-      scaleValue = scaleValue + 25;
-    } else {
-      scaleValue = 100;
-    }
-
-    cb(targetNode, scaleValue);
-  });
-
-  cb(targetNode, scaleValue);
-};
+  };
 
 })();
